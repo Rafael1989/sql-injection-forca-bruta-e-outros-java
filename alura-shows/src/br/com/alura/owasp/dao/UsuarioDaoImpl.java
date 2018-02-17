9fce5aeea861d74d5f68795547b5f97b87347016
rafael <rafael@rafael-Lenovo-G40-70>
2018-02-17 17:05:10 -0200

    Tirando sql injection com entity manager ou preparestatement

---
+++ alura-shows/src/br/com/alura/owasp/dao/UsuarioDaoImpl.java
@@ -0,0 +1,29 @@
+package br.com.alura.owasp.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import org.springframework.stereotype.Repository;
+
+import br.com.alura.owasp.model.Usuario;
+
+@Repository
+public class UsuarioDaoImpl implements UsuarioDao {
+	
+	@PersistenceContext
+	private EntityManager manager;
+
+
+	public void salva(Usuario usuario) {
+		manager.persist(usuario);
+	}
+
+	public Usuario procuraUsuario(Usuario usuario) {
+		TypedQuery<Usuario> query = manager.createQuery("select u from Usuario u where u.email=:email and u.senha=:senha",Usuario.class);
+		query.setParameter("email", usuario.getEmail());
+		query.setParameter("senha", usuario.getSenha());
+		Usuario usuarioEncontrado = query.getResultList().stream().findFirst().orElse(null);
+		return usuarioEncontrado;
+	}
+}
