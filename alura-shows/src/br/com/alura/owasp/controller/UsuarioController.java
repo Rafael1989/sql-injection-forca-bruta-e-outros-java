4ba35f3450dd61ac9b4b3228d747c3da0000a6c2
rafael <rafael@rafael-Lenovo-G40-70>
2018-02-18 17:10:19 -0300

    Se protegendo de mass assignment com Data Transfer Object (DTO) ou com setAllowedFields do initBinder do Spring...

--- alura-shows/src/br/com/alura/owasp/controller/UsuarioController.java
+++ alura-shows/src/br/com/alura/owasp/controller/UsuarioController.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,6 +21,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import br.com.alura.owasp.dao.UsuarioDao;
 import br.com.alura.owasp.model.Role;
 import br.com.alura.owasp.model.Usuario;
+import br.com.alura.owasp.model.UsuarioDTO;
 import br.com.alura.owasp.retrofit.GoogleWebClient;
 
 @Controller
@@ -30,6 +33,11 @@ public class UsuarioController {
 	
 	@Autowired
 	private GoogleWebClient googleWebClient;
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.setAllowedFields("nome","email","senha","nomeImagem");
+	}
 
 	@RequestMapping("/usuario")
 	public String usuario(Model model) {
@@ -49,6 +57,7 @@ public class UsuarioController {
 			RedirectAttributes redirect, HttpServletRequest request,
 			Model model, HttpSession session) throws IllegalStateException, IOException {
 
+		//Usuario usuarioRegistro = usuarioDTO.montaUsuario();
 		tratarImagem(imagem, usuarioRegistro, request);
 		usuarioRegistro.getRoles().add(new Role("ROLE_USER"));
 
